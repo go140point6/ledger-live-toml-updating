56a1b88f33568859facde6a86f136cee633f2222
gadget78 <crypto@gadet78.uk>
2024-04-12 00:03:19 +0100

    added node mode, along with other settings.

--- README.md
+++ README.md
@@ -1,87 +1,83 @@
 Use your validator to send information to your webhost to update your toml file, then create a landing page based on the data found in your toml file.
 
-No connection (no holes) to your validator, update every key ledger (or whenever you want)
+No connection (no holes) to your validator or node,
+
+update every key ledger (or whenever you want) via listener.py
+
+or timed intervals, by setting 'load-type' within in the update.py file, and loading direct, instead of listener.
+
 
 Be more transparent. Display your validator info, load, amendments, organization and principle. Hands free.
 
 Works on xrpl mainnet, xahau, testnet
 
-Xahau example here: https://xahau.validator.report/
+Xahau example here: https://xahau.validator.report/ or https://xahau.zerp.network
 
 Mainnet example here: https://mainnet.validator.report/ and here: https://mainnet2.validator.report/
 
 
-Setup Instructions - Validator and Web Host Configuration
+# Setup Instructions - Validator and Web Host Configuration
 
-
-
-Part 1: Validator Server Setup
+# Part 1: Validator/Node Server Setup
 
 Prerequisites:
 
 Python3, mpstat, free, df, awk
 
-Python3 requires:
+Python3 requires: json, websockets, subprocess, asyncio, requests
 
-json, websockets, subprocess, asyncio, requests
+`pip3 install websockets requests`
 
 Optional text editor: nano
 
-Webhost requires:
-
-PHP
-
-
 Upload Pre-existing Files: Upload update.py and listener.py to the validator server or use nano to create these files and paste the contents accordingly
 
-Editing update.py:
+### Editing update.py:
 Modify the following lines:
-xrpl = 'xahaud': Replace 'xahaud' with your XRPL node executable (e.g., "rippled")
-api_url = 'https://yourserver.com/toml.php': Replace with your API URL (The php you will upload)
-api_key = 'key': Replace 'key' with your desired API key. This must match the PHP script
+    xrpl = 'xahaud' # Replace with your XRPL node executable eg. "rippled" or "xahaud"
+    load_type = 'standalone' # 'standalone' when its loaded direct, it then uses a timer to trigger the update, 'listener' when being ran by the listener script to trigger the update.
+    mode = 'node' # 'validator' for validator type, so it checks/logs the AMMENDMENTS, and so it saves toml via API, 'node' has no ammendments and saves locally
+    wait_time = 60 # wait time before re-creating .toml (in seconds)
+    api_url = 'https://yourhost.com/toml.php'  # Replace with your API URL
+    api_key = 'key'  # Replace with your API key, this can be anything you want, you need to update the php script to match
+    file_path = '/home/www/.well-known/xahau.toml' # path to local .toml file, for use in node mode
+    allowlist_path = '/root/xahl-node/nginx_allowlist.conf' # allow list path, for use in connections output (node mode)
+    websocket_port = '6008' # port thats used for websocket (for use in connections, in node mode)
 
-Editing listener.py:
+### Editing listener.py for use in `load_type = 'listener' ``:
 Modify the line if necessary:
-uri = "ws://127.0.0.1:6009": Replace with the correct WebSocket server URI, you can find this in your validator config "port_ws_admin_local"
+    uri = "ws://127.0.0.1:6009": Replace with the correct WebSocket server URI, you can find this in your validator config "port_ws_admin_local"
 
 
+# Part 2: Web Host Setup
+
+Webhost requires:
+
+### PHP
+
+Editing toml.php, Change the following lines:
+
+    $allowedIPAddress = '0.0.0.0': Replace with your validator IP address to reject other sources
+    $apiKey = 'key': Set your API key (must match the one in update.py)
+    $filePath = '.well-known/xahau.toml': Change the file path as needed (xrp-ledger.toml for Mainnet)
+
+then Set file permissions to 644
 
 
-Part 2: Web Host Setup
-
-Editing toml.php:
-
-Change the following lines:
-
-$allowedIPAddress = '0.0.0.0': Replace with your validator IP address to reject other sources
-
-$apiKey = 'key': Set your API key (must match the one in update.py)
-
-$filePath = '.well-known/xahau.toml': Change the file path as needed (xrp-ledger.toml for Mainnet)
-
-Set file permissions to 644
-
-
-Editing index.html:
+### Editing index.html:
 
 Replace .well-known/xahau.toml with the correct TOML file path (use xrp-ledger.toml for Mainnet)
 
 
-Updating TOML File:
+# PART 3, Starting the Script, 
 
-Add the following lines to the bottom of your existing TOML file at .well-known/:
+To run the script,
 
+if you want to run in 'listener' mode type, have `load_mode = 'listener'` in the update.py setting, and do `nohup python3 listener.py &` this then updates the .toml file depending on the ledger action
 
-[[STATUS]]
+if you want to update the .toml file at regular intavals, set `load_mode = 'standalone'`, and adjust `wait_time = 60` in updater.py file, and then do `nohup python3 update.py` 
 
-[[AMENDMENTS]]
-
-Starting the Script:
-
-To run the script, type: nohup python3 listener.py &
-
-
-Stopping the Script:
+### Stopping the Script:
 
 Find the process ID with ps aux | grep python
 
--- validator/update.py
+++ validator/update.py
@@ -1,12 +1,20 @@
-import subprocess
+from datetime import datetime
 import json
 import requests
+import re
+import subprocess
+import time
 
 # You can change these variables to match your setup
 xrpl = 'xahaud' # Replace with your XRPL node executable eg. "rippled" or "xahaud"
+load_type = 'standalone' # 'standalone' when its loaded direct, it then uses a timer to trigger the update, 'listener' when being ran by the listener script to trigger the update.
+mode = 'node' # 'validator' for validator type, so it checks/logs the AMMENDMENTS, and so it saves toml via API, 'node' has no ammendments and saves locally
+wait_time = 60 # wait time before re-creating .toml (in seconds)
 api_url = 'https://yourhost.com/toml.php'  # Replace with your API URL
 api_key = 'key'  # Replace with your API key, this can be anything you want, you need to update the php script to match
-
+file_path = '/home/www/.well-known/xahau.toml' # path to local .toml file, for use in node mode
+allowlist_path = '/root/xahl-node/nginx_allowlist.conf' # allow list path, for use in connections output (node mode)
+websocket_port = '6008' # port thats used for websocket (for use in connections, in node mode)
 
 def run_command(command):
     try:
@@ -15,54 +23,78 @@ def run_command(command):
     except Exception as e:
         return str(e)
 
-def get_xrpl_server_info(key, time):
+def get_xrpl_server_info(key, timenow):
     try:
         server_info_result = subprocess.run([xrpl, "server_info"], capture_output=True, text=True)
         server_info_data = json.loads(server_info_result.stdout)
 
         status = server_info_data['result']['info']['server_state']
         version = server_info_data['result']['info']['build_version']
-        status_time = server_info_data['result']['info']['server_state_duration_us']
+        status_time = int(server_info_data['result']['info']['server_state_duration_us']) / 1000000
         node_size = server_info_data['result']['info']['node_size']
+        ledger = server_info_data['result']['info']['validated_ledger']['seq']
         ledgers = server_info_data['result']['info']['complete_ledgers']
-        # Mainnet doesn't provide a network id, so default 0
-        network = server_info_data['result']['info'].get('network_id', 0)
+        peers = server_info_data['result']['info']['peers']
+        network = server_info_data['result']['info'].get('network_id', 0) # Mainnet doesn't provide a network id, so default 0
+
+        uptime_in_seconds = server_info_data['result']['info']['uptime']
+        days = uptime_in_seconds // 86400
+        hours = (uptime_in_seconds % 86400) // 3600
+        minutes = (uptime_in_seconds % 3600) // 60
+        formatted_uptime = f"{days} Days, {str(hours).zfill(2)} Hours, and {str(minutes).zfill(2)} Mins"
+        
         cpu_usage = run_command("mpstat 1 1 | awk '/Average:/ {print 100 - $12\"%\"}'")
         ram_usage = run_command("free | awk '/Mem:/ {printf(\"%.2f%\"), $3/$2 * 100}'")
         disk_usage = run_command("df -h . | awk 'NR==2{print $5}'")
         swap_usage = run_command("free | awk '/Swap:/ {printf(\"%.2f%\"), $3/$2 * 100}'")
 
-        feature_result = subprocess.run([xrpl, "feature"], capture_output=True, text=True)
-        feature_data = json.loads(feature_result.stdout)
-        amendments = feature_data['result']['features']
-        filtered_amendments = {
-            value['name']: key
-            for key, value in amendments.items()
-            if value.get('enabled') == False and value.get('supported') == True and value.get('vetoed') == False
-        }
+        if type == 'validator':
+            feature_result = subprocess.run([xrpl, "feature"], capture_output=True, text=True)
+            feature_data = json.loads(feature_result.stdout)
+            amendments = feature_data['result']['features']
+            filtered_amendments = {
+                value['name']: key
+                for key, value in amendments.items()
+                if value.get('enabled') == False and value.get('supported') == True and value.get('vetoed') == False
+            }
+            amendments_output = "\n".join([f"{name} = \"{id}\"" for name, id in filtered_amendments.items()])
+
+            websocket_connections = 0
+            allowlist_count = 0
+
+        else:
+            amendments_output = ledger
+            websocket_connections = run_command( "netstat -an | grep " + websocket_port + " | wc -l | awk '{print int(($1 - 1) / 2)}'" ) # we subtract 1 (the node itself) and then divide by two, as it lists the proxy AND node seperately
+            allowlist_count = int(run_command ( "wc -l " + allowlist_path + " | awk '{print $1}' " )) - 2 # we -2 here as 2 entries out of the 3 default entries that are created on install are for the node itself
+
 
         status_output = f"""
+STATUS = "{status}"
+BUILDVERSION = "{version}"
+LASTREFRESH = "{timenow}Z UTC"
+UPTIME = "{formatted_uptime}"
+STATUSTIME = "{status_time} in seconds"
+CURRENTLEDGER = "{ledger}"
+LEDGERS = "{ledgers}"
+NODESIZE = "{node_size}"
+NETWORK = "{network}"
+CONNECTIONS = "{websocket_connections}/{allowlist_count}"
+PEERS = "{peers}"
+
 CPU = "{cpu_usage}"
 RAM = "{ram_usage}"
 DISK = "{disk_usage}"
 SWAP = "{swap_usage}"
-STATUS = "{status}"
-STATUSTIME = "{status_time}"
-BUILDVERSION = "{version}"
-NODESIZE = "{node_size}"
-LEDGERS = "{ledgers}"
-NETWORK = "{network}"
+
 KEY = "{key}"
-TIME = "{time}"
 """
-
-        amendments_output = "\n".join([f"{name} = \"{id}\"" for name, id in filtered_amendments.items()])
-
-        return {
-            'STATUS': status_output,
-            'AMENDMENTS': amendments_output
-        }
+        if mode == 'validator':
+            return { 'STATUS': status_output, 'AMENDMENTS': amendments_output }
+        else:
+            return status_output
+    
     except Exception as e:
+        print("oops: error with creating status_ouput")
         return str(e)
 
 def send_to_api(data):
@@ -83,13 +115,43 @@ def send_to_api(data):
     except requests.exceptions.RequestException as err:
         print("Oops: Something Else", err)
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage: script.py <KEY> <TIME>")
-    else:
-        key = sys.argv[1]
-        time = sys.argv[2]
-        info = get_xrpl_server_info(key, time)
-        send_to_api(info)
+def update_toml_file(info, utcnow):
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    updated_content = re.sub(
+        r'\[\[STATUS\]\].*?\[\[AMENDMENTS\]\]',
+        '[[STATUS]]' + info + '\n[[AMENDMENTS]]',
+        file_content,
+        flags=re.DOTALL
+    )
+    updated_content = re.sub(
+        r'^modified = .*',
+        f'modified = {utcnow.strftime("%Y-%m-%dT%H:%M:%S.%fZ")}',
+        updated_content,
+        flags=re.M
+    )
 
+    #print(updated_content)
+    with open(file_path, 'w') as file:
+        file.write(updated_content)
+
+if __name__ == "__main__":
+    
+    if mode == 'validator':
+        while True:
+            import sys
+            if len(sys.argv) != 3 & load_type == 'listener':
+                print("Usage: update.py <KEY> <TIME>")
+            else:
+                key = sys.argv[1]
+                timearg = sys.argv[2]
+                info = get_xrpl_server_info(key, timearg)
+                send_to_api(info)
+            if load_type == 'listener': break
+            else: time.sleep(wait_time)
+
+    if mode == 'node':
+        while True:
+            info = get_xrpl_server_info(False, datetime.utcnow())
+            update_toml_file(info, datetime.utcnow())
+            time.sleep(wait_time)
